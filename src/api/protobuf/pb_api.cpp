a4abf317fe4a9d7042304b166ecb09f4f4af0204
as-iotex <as@iotex.io>
2021-06-08 15:50:17 +0100

    Fix SetValueFromJsonObject error handling

--- src/api/protobuf/pb_api.cpp
+++ src/api/protobuf/pb_api.cpp
@@ -33,7 +33,7 @@ namespace
                 break;
 
             default:
-                ResultCode::ERROR_JSON_PARSE;
+                return ResultCode::ERROR_JSON_PARSE;
             }
 
         // Set the value
