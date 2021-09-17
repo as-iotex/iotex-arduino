870757fd9eb23cef7dd1d931637817a699c56013
as-iotex <>
2021-09-17 08:47:30 +0100

    Fix wallet unit tests

--- tests/src/api/wallets/walletsTests.cpp
+++ tests/src/api/wallets/walletsTests.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include "secrets.h"
+
 #include "api/wallet/wallets.h"
 #include "connection/connection.h"
 
@@ -12,8 +14,8 @@ using namespace Iotex::ResponseTypes;
 
 namespace 
 {
-    constexpr const char tTransactionHash[] = "ac675a8440e6e30c3b9dcfb810b772e2ba9e2c1d281db0c1f4bee073b35f890c";
-    constexpr const char tAccount[] = "io1xkx7y9ygsa3dlmvzzyvv8zm6hd6rmskh4dawyu";
+    constexpr const char tTransactionHash[] = "e19dfb0c84799fc43217287d0d81369348279a0b3b32d0ad2f973ee5aaa392ae";
+    constexpr const char tAccount[] = IOTEX_ADDRESS_2;
     constexpr const char tIp[] = "gateway.iotexlab.io";
     constexpr const char tBaseUrl[] = "iotexapi.APIService";
     constexpr const int tPort = 10000;
@@ -33,7 +35,7 @@ TEST_F(WalletTests, GetBalance)
     ResultCode result = connection.api.wallets.getBalance(tAccount, balance);
     
     ASSERT_EQ(result, ResultCode::SUCCESS);
-    ASSERT_EQ(balance, "888000000000000000000");
+    ASSERT_EQ(balance, "1000000000000000000");
 }
 
 TEST_F(WalletTests, GetAccount)
@@ -44,7 +46,7 @@ TEST_F(WalletTests, GetAccount)
 
     ASSERT_EQ(result, ResultCode::SUCCESS);
     ASSERT_STREQ(accountMeta.address, tAccount);
-    ASSERT_STREQ(accountMeta.balance, "888000000000000000000");
+    ASSERT_STREQ(accountMeta.balance, "1000000000000000000");
     ASSERT_FALSE(accountMeta.isContract);
     ASSERT_EQ(accountMeta.nonce, "0");
     ASSERT_EQ(accountMeta.pendingNonce, "1");
@@ -59,21 +61,21 @@ TEST_F(WalletTests, GetActionByHash)
 
     ASSERT_EQ(result, ResultCode::SUCCESS);
     ASSERT_EQ(transaction.action.core.version, 0);
-    ASSERT_EQ(transaction.action.core.nonce, 1);
+    ASSERT_EQ(transaction.action.core.nonce, 19);
     ASSERT_EQ(transaction.action.core.gasLimit, 21000);
     ASSERT_STREQ(transaction.action.core.gasPrice, "1000000000000");
-    ASSERT_STREQ(transaction.action.core.transfer.amount, "888000000000000000000");
-    ASSERT_STREQ(transaction.action.core.transfer.recipient, "io1xkx7y9ygsa3dlmvzzyvv8zm6hd6rmskh4dawyu");
+    ASSERT_STREQ(transaction.action.core.transfer.amount, "1000000000000000000");
+    ASSERT_STREQ(transaction.action.core.transfer.recipient, "io1w3egd539q90jauqumvg09v3kv2hpqsy2mwym0q");
     ASSERT_EQ(transaction.action.core.transfer.payload, "");
     ASSERT_STREQ(transaction.action.senderPublicKey, "BBEUzF7ETUgLUaIoWXHUELMR1nYf3XLxSMKfIRsuJC1+oUuKtDptd4rIVc5lADqeEGl2Wme0YKQHyP22IPmEB5A=");
-    ASSERT_STREQ(transaction.action.signature, "mbWTQyW19tGwOv1qXr/0FZAiiX3xwbeklAnopz5qz8l5Wp+zYqxThQH3LBtWqFsRZ8PfmCouuCIgZEaZYOhiaBw=");
+    ASSERT_STREQ(transaction.action.signature, "WC9/nl3gpnIvt4HdibVGG4gZrAolg+c1hFQ5sL5e22US92ymx9QZiiaEO3s2/fDCPamjuAxP2IF4dl7zs5XfqRs=");
     
-    ASSERT_STREQ(transaction.actHash, "ac675a8440e6e30c3b9dcfb810b772e2ba9e2c1d281db0c1f4bee073b35f890c");
-    ASSERT_STREQ(transaction.blkHash, "6efbb4bdaa0ce4c8b45417fbf87d545a3c0c174d432b8d1005d588d89a98faf5");
-    ASSERT_EQ(transaction.blkHeight, "8755936");
+    ASSERT_STREQ(transaction.actHash, "e19dfb0c84799fc43217287d0d81369348279a0b3b32d0ad2f973ee5aaa392ae");
+    ASSERT_STREQ(transaction.blkHash, "81c03186a0374154a6c898e64a675d33784b2086006790afa0ebc2591be97d08");
+    ASSERT_EQ(transaction.blkHeight, "13208023");
     ASSERT_STREQ(transaction.sender, "io1l5cqp670fmqgyfttux85e3y362jxnlmpskgeus");
     ASSERT_STREQ(transaction.gasFee, "10000000000000000");
-    ASSERT_STREQ(transaction.timestamp, "2021-05-15T16:39:45Z");
+    ASSERT_STREQ(transaction.timestamp, "2021-09-14T07:14:40Z");
    
 }
 
