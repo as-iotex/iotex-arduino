7a471da1b18d435bd1d6dbe8f4c69a1a32eb91c9
as-iotex <>
2021-09-26 17:03:42 +0100

    Add library.properties and main library header

---
+++ src/iotex-client.h
@@ -0,0 +1,20 @@
+#ifndef IOTEX_CLIENT_H
+#define IOTEX_CLIENT_H
+
+#include "constants.h"
+#include "result_codes.h"
+#include "api/wallet/wallets.h"
+#include "api/wallet/wallets.h"
+#include "connection/connection.h"
+#include "account/account.h"
+#include "random/random.h"
+#include "storage/storage.h"
+#include "contract/contract.h"
+#include "helpers/client_helpers.h"
+
+using namespace Iotex;
+using namespace Iotex::api;
+using namespace Iotex::abi;
+using namespace Iotex::ResponseTypes;
+
+#endif
